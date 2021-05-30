bb161c367ded31a617466ebaea38670dca4053ad
HACKERPROTM <brandontorreglosa@gmail.com>
2021-05-30 11:52:38 +0300

    node modules bye bye

--- node_modules/images-scraper/src/index.js
+++
@@ -1,3 +0,0 @@
-const ScraperClass = require('./google/scraper');
-ScraperClass.default = ScraperClass;
-module.exports = ScraperClass;
--- node_modules/images-scraper/src/logger.js
+++
@@ -1,9 +0,0 @@
-const winston = require('winston');
-
-const consoleTransport = new winston.transports.Console();
-
-const winstonOptions = {
-  level: process.env.LOG_LEVEL || 'info',
-  transports: [consoleTransport],
-};
-module.exports = new winston.createLogger(winstonOptions);
