da3692ce74fcd9e5ef765ec6e041662bb183e556
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-05 16:58:21 +0300

    update

---
+++ commands/math.js
@@ -0,0 +1,19 @@
+const math = require('mathjs');
+
+module.exports = {
+    name: math,
+    permissions: ["SEND_MESSAGES"],
+    aliases: ['mathprob', 'mathproblem'],
+    premium: true,
+async execute(client, message, cmd, args, Discord) {
+    try {
+        message.channel.send(
+            new MessageEmbed()
+            .addField("Question", args.join(" "))
+            .addField("Solution", math.evaluate(args.join(" ")))
+        );
+    } catch (err) {
+        message.channel.send("***Your Question Is Not A Valid Equation! \nMade By `👑HACKERPROᵈᵉᵛ#1498`");
+    }
+},
+};
