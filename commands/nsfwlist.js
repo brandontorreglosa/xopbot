eb8ad5eb88bdb181b0fc525524623c441ca42b9d
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-07 14:57:29 +0300

    Update nsfwlist.js

--- commands/nsfwlist.js
+++ commands/nsfwlist.js
@@ -4,15 +4,22 @@ module.exports = {
     permissions: ["SEND_MESSAGES"],
     execute(client, message, cmd, args, Discord) {
 
-        if (!message.channel.nsfw) return message.channel.send('**This Is Not A NSFW Channel! 🔞**') 
+
+        var errMessage = "**This Is Not A NSFW Channel! 🔞**";
+        if (!message.channel.nsfw) {
+            message.react('💢');
+
+            return message.reply(errMessage)
+        .then(message => {
+            message.delete({ timeout: 3000 })
+        })
+    }
 
         const newEmbed = new Discord.MessageEmbed()
         .setColor('#c30202')
         .setTimestamp()
-        .setTitle('NSFW List')
-        .addFields(
-            {name: '🔞 NSFW', value: '`4k,hentai,neko,pgif`'}
-        )
+        .setTitle('🔞 NSFW List')
+        .setDescription('`4k,hentai,neko,pgif`')
         .setFooter('Bot Developer @👑HACKERPRO™#9999');
 
         message.channel.send(newEmbed);
