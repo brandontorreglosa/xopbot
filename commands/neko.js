b2b2816ef468c9fbbaf93ba34c22de92190d9f97
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-07 14:24:45 +0300

    updated

---
+++ commands/neko.js
@@ -0,0 +1,29 @@
+const Discord = require("discord.js");
+module.exports = {
+name: '4k',
+permissions: ["SEND_MESSAGES"],
+async execute(client, message, cmd, args, Discord) {
+
+    var superagent = require('superagent');
+
+    if (!message.channel.nsfw) return message.channel.send('**This Is Not A NSFW Channel! 🔞**') 
+
+    var lo = new Discord.MessageEmbed()
+                .setDescription(`Sending Neko...`)
+                .setTimestamp()
+
+    message.channel.send(lo).then(m => {
+
+        superagent.get('https://nekobot.xyz/api/image').query({ type: 'neko'}).end((err, response) => {
+
+            var embed_nsfw = new Discord.MessageEmbed()
+                .setDescription(`:underage:\n**[Provided To You By The Bot Supporters Of XOPBOT](${response.body.message})**`)
+                .setTimestamp()
+                .setImage(response.body.message)
+                .setFooter('Nice Huh :)')
+            
+            m.edit(embed_nsfw);
+        });
+    });
+}
+}
