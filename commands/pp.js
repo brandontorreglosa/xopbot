bec22e050d24aabada19802da15273d85a205c2b
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-08 16:29:58 +0300

    update pp

---
+++ commands/pp.js
@@ -0,0 +1,50 @@
+module.exports = {
+    name: 'pp',
+    permissions: ["SEND_MESSAGES"],
+    aliases: ['ppsize'],
+    async execute(client, message, cmd, args, Discord) {
+
+        const pp = [
+            '8D',
+            '8=D',
+            '8==D',
+            '8===D',
+            '8====D',
+            '8=====D',
+            '8======D',
+            '8=======D',
+            '8========D',
+            '8=========D',
+            '8==========D',
+            '8===========D',
+            '8============D',
+            '8=============D',
+            '8==============D',
+            '8===============D',
+            '8================D',
+            '8=================D',
+            '8==================D',
+            '8===================D',
+            '8====================D',
+            '8=====================D',
+            '8======================D',
+            '8=======================D',
+            '8========================D',
+            '8=========================D'
+        ];
+
+      const member =  message.mentions.members.first() || message.guild.members.cache.get(args[0]) || message.member;
+
+      if(!member)
+      return message.reply(`***Mention Someone Or Provide Their User ID To Get Their PP Size!***`)
+
+      const embed = new Discord.MessageEmbed()
+      .setTitle('PP Size Detector')
+      .setDescription(`${member.displayName}'s PP Is This Big\n\`\`\`${pp[Math.floor(Math.random() * pp.length)]}\`\`\``)
+      .setFooter(`Requested By ${message.author.username}`,  message.author.displayAvatarURL({ dynamic: true }))
+      .setTimestamp()
+      .setColor(member.displayHexColor);
+
+      await message.channel.send(embed)
+    }
+}
