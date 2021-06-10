f5d6235dd765b8fc0dfe057314b69dc4cf8d2bf2
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-10 07:08:38 +0300

    update

--- commands/nsfwlist.js
+++ commands/nsfwlist.js
@@ -20,8 +20,8 @@ module.exports = {
         .setColor('#c30202')
         .setTimestamp()
         .setTitle('🔞 NSFW List')
-        .setDescription('Real Life NSFW: `4k` `neko` `pgif` `anal` `ass` `pussy` `boobs` `thigh` \nAnime NSFW: `hentai` `hthigh` `hanal` `hboobs` `hneko` `hkitsune`')
-        .setFooter('Bot Developer @👑HACKERPRO™#9999');
+        .setDescription('Real Life NSFW: `4k` `neko` `pgif` `anal` `ass` `pussy` `boobs` `thigh` \nAnime NSFW: `hentai` `hthigh` `hanal` `hboobs` `hneko` `hkitsune` \nBot Invite: (https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295)')
+        .setFooter('Bot Developer 👑HACKERPROᵈᵉᵛ#1498');
 
         message.channel.send(newEmbed);
     }
