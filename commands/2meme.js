f5177f8b76207528f566fa51e767f97bda210e20
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-10 17:33:26 +0300

    Create 2meme.js

---
+++ commands/2meme.js
@@ -0,0 +1,27 @@
+const got = require('got');
+
+module.exports = {
+    name: "2meme",
+    category: "Image",
+    description: "Sends a random meme from reddit",
+
+    async execute(client, message, cmd, args, Discord) {
+        const embed = new Discord.MessageEmbed()
+        got('https://reddit.com/r/dankmemes/random.json').then(response => {
+            let content = JSON.parse(response.body);
+            let permalink = content[0].data.children[0].data.permalink;
+            let memeUrl = `https://reddit.com${permalink}`;
+            let memeImage = content[0].data.children[0].data.url;
+            let memeTitle = content[0].data.children[0].data.title;
+            let memeUpvotes = content[0].data.children[0].data.ups;
+            let memeDownvotes = content[0].data.children[0].data.downs;
+            let memeNumComments = content[0].data.children[0].data.num_comments;
+            embed.setTitle(`${memeTitle}`)
+            embed.setURL(`${memeUrl}`)
+            embed.setImage(memeImage)
+            embed.setColor('RANDOM')
+            embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+            message.channel.send(embed);
+        })
+
+}    }
