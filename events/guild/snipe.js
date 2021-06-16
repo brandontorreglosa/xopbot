5938019b342e6adf83b8f1346d93f1f86ccc468c
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-16 16:47:46 +0300

    Update snipe.js

--- events/guild/snipe.js
+++ events/guild/snipe.js
@@ -1,13 +1,13 @@
-const client = require('../../main');
-client.on('messageDelete', (message) => {
-    let snipes = client.snipes.get(message.channel.id) || [];
-    if (snipes.length > 5) snipes = snipes.slice(0, 4);
+// const client = require('../../main');
+// client.on('messageDelete', (message) => {
+//     let snipes = client.snipes.get(message.channel.id) || [];
+//     if (snipes.length > 5) snipes = snipes.slice(0, 4);
 
-    snipes.unshift({
-        msg: message,
-        image: message.attachments.first()?.proxyURL || null,
-        time: Date.now(),
-    });
+//     snipes.unshift({
+//         msg: message,
+//         image: message.attachments.first()?.proxyURL || null,
+//         time: Date.now(),
+//     });
 
-    client.snipes.set(message.channel.id, snipes);
-})
+//     client.snipes.set(message.channel.id, snipes);
+// })
