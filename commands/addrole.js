0e4ff5fb47ac9e813395f4d2df2445375180a4a6
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-16 11:22:11 +0300

    update roles command

--- commands/addrole.js
+++ commands/addrole.js
@@ -6,7 +6,7 @@ module.exports = {
       const member = message.mentions.members.first()
       if(!member) {
           const addroleError = new Discord.MessageEmbed()
-          .setDescription(`Please mention a member in order to give them the role`)
+          .setDescription(`**Please Mention A Member In Order To Give Them The Role!**`)
           .setColor("RED")
           return message.channel.send(addroleError)
       }
@@ -15,7 +15,8 @@ module.exports = {
       
       if(!roleToGive) {
           const addroleError2 = new Discord.MessageEmbed()
-          .setDescription(`No Roles Provided`)
+          .setTimestamp()
+          .setDescription(`**No Roles Provided!**`)
           .setColor("RED")
           return message.channel.send(addroleError2)
       }
@@ -24,51 +25,27 @@ module.exports = {
 
       if(selfPosition <= mentionedPosition) {
           const posi = new Discord.MessageEmbed()
-          .setDescription(`You cannot add role to this member as their role is higher/equal to yours.`)
+          .setTimestamp()
+          .setDescription(`**You Cannot Add Role To ${member} As Their Role Is Higher/Equal To Yours!**`)
           .setColor("RED")
           return message.channel.send(posi)
       }
       if(member.roles.cache.get(roleToGive.id)) {
           const addroleError3 = new Discord.MessageEmbed()
-          .setDescription(`The member already has that role`)
+          .setTimestamp()
+          .setDescription(`**${member} Already Has That Role!**`)
           .setColor("RED")
           return message.channel.send(addroleError3)
       }
       member.roles.add(roleToGive)
       const embed = new Discord.MessageEmbed()
-      .setDescription(`Role ${roleToGive} has been added to ${member}`)
+      .setTimestamp()
+      .setDescription(`The Role ${roleToGive} Has Been Added To ${member}`)
       .setColor("BLUE")
+      .setFooter(`Requested By: ${message.author.tag} \nIf The Role Is Higher Than The Bots Order \nThe Bot Wont Be Able To Add That Role!`, message.author.displayAvatarURL())
 
       message.channel.send(embed)
 
       
   }
 }
-//         if (!message.member.hasPermission("MANAGE_ROLES")) {
-//             return message.channel.send("**Sorry You Need Permissions To Add A Role!**");
-//           }
-//           if (!message.guild.me.hasPermission("MANAGE_ROLES")) {
-//             return message.channel.send("**I Do Not Have Permission To Manage Roles!**");
-//           } 
-//         let target = message.mentions.members.first();
-    
-//         if(!target) return message.reply(`**Please Mention A User!**`)
-        
-//         let arole = message.mentions.roles.first();
-        
-//         if(!arole) return message.reply(`**Please Mention Role For Add!**`)
-        
-//         //let ticon = target.user.avatarURL({ dynamic: true, size: 2048 });
-//         //let aicon = message.author.avatarURL({ dynamic: true, size: 2048 });
-        
-//           const embed = new Discord.MessageEmbed()
-//           .setTitle(`${target.user.username} Has Been Given A Role`)
-//           .setTimestamp()
-//           .setColor("#c30202")
-//           .setDescription(`**XOPBOT Changed Role For ${target.user.username} And Added ${arole}**`)
-//           .setFooter('👑HACKERPROᵈᵉᵛ#1498')
-//          await message.channel.send(embed)
-          
-//           target.roles.add(arole)
-//     }
-// }
