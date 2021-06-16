7db99c442c7ff655bde8aa68f82835f6c5e27f98
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-16 16:13:01 +0300

    snipe command

---
+++ commands/snipe.js
@@ -0,0 +1,23 @@
+const moment = require('moment');
+
+module.exports = {
+    name: 'snipe',
+    permissions: ["MANAGE_MESSAGES"],
+    async execute(client, message, cmd, args, Discord) {
+        const snipes = client.snipes.get(message, channel.id);
+        if(!snipes) return message.reply('**XOPBOT Can Not Find Any Messages Recently Deleted In This Channel!**');
+
+        const snipe = +args[0] - 1 || 0;
+        const target = snipes[snipe];
+        if(!target) return message.reply(`**There Is Only ${snipes.length} Messages!**`);
+
+        const { msg, time, image} = target;
+        const embed = new Discord.MessageEmbed()
+        .setTitle(`OMG ${target} Got Sniped Down`)
+        .setDescription(msg.content)
+        .setAuthor(msg.author.tag, msg.author.displayAvatarURL())
+        .setImage(image)
+        .setFooter(`${moment(time).fromNow()} | ${snipe + 1} / ${snipes.length}`)
+        .setColor("RED")
+    }
+}
---
+++ events/guild/snipe.js
@@ -0,0 +1,13 @@
+const Client = require('../main');
+client.on('messageDelete', (message) => {
+    let snipes = client.snipes.get(message.channel.id) || [];
+    if (snipes.length > 5) snipes = snipes.slice(0, 4);
+
+    snipes.unshift({
+        msg: message,
+        image: message.attachments.first()?.proxyURL || null,
+        time: Date.now(),
+    });
+
+    client.snipes.set(message.channel.id, snipes);
+})
