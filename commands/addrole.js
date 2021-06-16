e4bc66923ba6835a29341428dc3d8a09ba144194
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-16 11:31:37 +0300

    Update addrole.js

--- commands/addrole.js
+++ commands/addrole.js
@@ -40,9 +40,10 @@ module.exports = {
       member.roles.add(roleToGive)
       const embed = new Discord.MessageEmbed()
       .setTimestamp()
+     // .setTitle(`The Role ${roleToGive} Has Been Added To ${member}`)
       .setDescription(`The Role ${roleToGive} Has Been Added To ${member}`)
       .setColor("BLUE")
-      .setFooter(`Requested By: ${message.author.tag} \nIf The Role Is Higher Than The Bots Order \nThe Bot Wont Be Able To Add That Role!`, message.author.displayAvatarURL())
+     // .setFooter(`Requested By: ${message.author.tag} \nIf The Role Is Higher Than The Bots Order \nThe Bot Wont Be Able To Add That Role!`, message.author.displayAvatarURL())
 
       message.channel.send(embed)
 
