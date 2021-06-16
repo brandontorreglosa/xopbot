6615ca0d5b6789cd97621dcadbbc6f08027c76b8
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-16 16:55:04 +0300

    fix bot please

--- main.js
+++ main.js
@@ -1,7 +1,7 @@
 const Discord = require('discord.js');
 const Levels = require('discord-xp');
 require('dotenv').config();
-const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTIONS", "COLLECTION"]});
+const client = new Discord.Client({partials: ["MESSAGE", "CHANNEL", "REACTIONS"]});
 const mongoose = require('mongoose');
 //const { GiveawayCreator } = require('discord-giveaway');
 //const Creator = new GiveawayCreator(client, 'mongoose');
@@ -10,7 +10,7 @@ const fs = require('fs')
 
 client.commands = new Discord.Collection();
 client.events = new Discord.Collection();
-client.snipes = new Discord.Collection();
+//client.snipes = new Discord.Collection();
 //client.giveaways = Creator;
 
 ['command_handler', 'event_handler'].forEach(handler =>{
