2cdd51145e957f1288c2e77a97993fe30f919ea9
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-21 20:32:09 +0300

    update v50.1.3

---
+++ commands/say.js
@@ -0,0 +1,12 @@
+module.exports = {
+    name: 'say',
+    permissions: ["SEND_MESSAGES"],
+    description: "Make the bot say your message",
+    async execute(client, message, cmd, args, Discord) {
+        if (!args.join(" ")) {
+        message.channel.send("**Please Add Some Text for Me XOPBOT To Repeat! \nAnything Rude Is Sended To The Developer!**")
+        }
+        message.channel.send(args.join(" "), { allowedMentions: { parse:["users"] } });
+     message.delete(10000);
+    }
+}
