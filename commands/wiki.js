4f2f4634356b187b12ea24b1f5e0b4381ad5f8c2
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-21 11:52:00 +0300

    update v50.0.5

---
+++ commands/wiki.js
@@ -0,0 +1,23 @@
+const Discord = module.require("discord.js");
+
+module.exports = {
+    name: "wiki",
+    permissions: ["SEND_MESSAGES"],
+    description: "Get Search Results from Wikipedia",
+     async execute(client, message, cmd, args, Discord)  {
+        const search = args.join("_");
+        const msg = args.join(" ");
+        if (!msg) {
+            return message.channel.send("**You Need To Enter Some Text to Search For!**")
+        }
+        const link = `https://www.wikipedia.org/w/index.php?search=${search}&ns0=1`;
+        const embed = new Discord.MessageEmbed()
+        .setTimestamp()
+        .setTitle("Wikipedia Search")
+        .addField(`You Searched For:`, `${msg}`)
+        .addField(`Results:`, `[Here's What I Found](${link})`)
+        .setColor("RANDOM");
+
+        message.channel.send(embed);
+    }
+}
