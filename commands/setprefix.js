4468ac6e00a6111ec3bde34380e57196dca3108d
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-25 16:57:19 +0300

    Update setprefix.js

--- commands/setprefix.js
+++ commands/setprefix.js
@@ -6,10 +6,10 @@ module.exports = {
     permissions: ["ADMINISTRATOR"],
    async execute(client, message, cmd, args, Discord){
 
-      if(!args[0]) return msg.channel.send(":warning: Please specify a prefix!"); //If there isn't a prefix then return a message
+      if(!args[0]) return message.channel.send(":warning: Please specify a prefix!"); //If there isn't a prefix then return a message
       
       let prefixes = JSON.parse(fs.readFileSync("./prefixes.json")); //Read File
-      prefixes[msg.guild.id] = { //Let The config be
+      prefixes[message.guild.id] = { //Let The config be
       prefix: args[0] //Let prefix = arguement 1
       }
       
@@ -17,7 +17,7 @@ module.exports = {
         if(err) console.log(err); //If error log error to the console
       })
       
-      msg.channel.send(`Prefix has been set to **${args[0]}**!`); //send message to that channel
+      message.channel.send(`Prefix has been set to **${args[0]}**!`); //send message to that channel
       return; //return
     }
 }
