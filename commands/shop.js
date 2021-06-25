767ad51f23ef05d2d98eabee432298ddead8bb7a
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-25 10:04:11 +0300

    economy update

---
+++ commands/shop.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+const items = require("../../shopitems")
+
+module.exports = {
+    name: 'shop',
+    permissions: ["SEND_MESSAGES"],
+    async execute(client, message, cmd, args, Discord) {
+        if (items.length === 0) return message.reply("**There Is Currently Nothing In Xopbots Shop!**");
+
+        const shopList = items.map((value, index) => {
+            return `**${index + 1})** ${value.item} -> **${value.coins}** Xocoins!`;
+        });
+
+        message.channel.send(shopList);
+    }
+}
---
+++ shopitems.js
@@ -0,0 +1,26 @@
+module.exports = [
+    {
+        item: "Gun",
+        coins: 2000,
+    },
+    {
+        item: "Submachine",
+        coins: 3500,
+    },
+    {
+        item: "Minigun",
+        coins: 5000,
+    },
+    {
+        item: "Rocket",
+        coins: 10000,
+    },
+    {
+        item: "Bananagun",
+        coins: 20000,
+    },
+    {
+        item: "Sniper",
+        coins: 30000,
+    }
+]
