907018e9e79c6946991b2798492f68c2599e905b
HACKERPROTM <brandontorreglosa@gmail.com>
2021-06-28 16:44:59 +0300

    update images v8

---
+++ commands/tableflip.js
@@ -0,0 +1,20 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "tableflip",
+    permissions: ["SEND_MESSAGES"],
+    cooldown: 3,
+    description: "Image Manipulation Command",
+    async execute(client, message, cmd, args, Discord) {
+
+        const user = message.mentions.members.first()
+    if (!user) {
+        return message.channel.send("**Please Mention A User!**")
+    }
+
+    const avatar = user.user.displayAvatarURL({ size: 2048, format: "png" });
+
+
+        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/tableflip?user=${avatar}`, name: "grave.png"}]});
+    }
+    }
