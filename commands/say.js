9959c83734d6d9a4a2737ccfb6ffcfc4219be053
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-01 12:26:43 +0300

    Update say.js

--- commands/say.js
+++ commands/say.js
@@ -4,11 +4,19 @@ module.exports = {
     description: "Make the bot say your message",
     async execute(client, message, cmd, args, Discord) {
 
+        const channel = client.channels.cache.get('860085472944848927')
+
         const user = message.mentions.users.first();
 
-        if (message.content.match("gore") || message.content.match("dis") || message.content.match("kls")) {
+        if (message.content.match("gore") || message.content.match("fuck") || message.content.match("shit") || message.content.match("nigga") || message.content.match("hoe") || message.content.match("bitch") || message.content.match("dick") || message.content.match("gay") || message.content.match("lesbian") || message.content.match("blowjob") || message.content.match("porn") || message.content.match("cunt")) {
             message.delete();
             message.reply(`**You Have Been Reported To The Developer!**`);
+            const embed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setTitle(`Author \n${message.author.username} Used Say!`)
+            .setDescription(`Guild \nIn ${message.guild.name} \nHe Commanded This Word ${sayMessage}`)
+            channel.send(embed);
+        }
     
         if (!args[0]) {
         message.channel.send("**Please Add Some Text for Me XOPBOT To Repeat! \nAnything Rude Is Sended To The Developer!**")
@@ -18,6 +26,5 @@ module.exports = {
 
         message.channel.send(`${sayMessage}`, { allowedMentions: { parse:["users"] } });
     //  message.delete(10000);
-    }
 }
 }
