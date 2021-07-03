5e9dfbed7cd11e0758d0f07332fcbf1ac0fc1eef
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-03 20:00:50 +0300

    Update pet.js

--- commands/pet.js
+++ commands/pet.js
@@ -8,7 +8,7 @@ module.exports = {
     async execute(client, message, cmd, args, Discord) {
 
     const mention = message.mentions.members.first() || message.member;
-    const avatar = mention.user.displayAvatarURL({size: 2048, format: "gif"});
+    const avatar = mention.user.displayAvatarURL({size: 2048, format: "png"});
 
 
         message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/pet?image=${avatar}`, name: "xopbotpet.gif"}]});
