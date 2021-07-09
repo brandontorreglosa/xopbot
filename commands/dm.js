69aa23e12ad5d72551b6915270258da0ad01c6b0
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-09 18:36:00 +0300

    Create dm.js

---
+++ commands/dm.js
@@ -0,0 +1,17 @@
+module.exports = {
+    name: 'dm',
+    permissions: ["MANAGE_MESSAGES"],
+    cooldown: 15,
+    async execute(client, message, cmd, args, Discord) {
+        try {
+        var mentions = msg.mentions.users.first();
+                var dmMessage = args.slice(0).join(' ');
+                if(!dmMessage) return message.channel.send(`${message.author.username} Couldnt Send Without A Message!`)
+                mentions.send(`**${dmMessage}**`) 
+            }  catch (err) {
+
+                const errorlogs = client.channels.cache.get(errorChannel)
+                errorlogs.send(`Error On Bot Server Command!\n\nError:\n\n ${err}`)
+            }
+        }
+        }
