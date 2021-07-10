4e8814779e34f6bf8fb495e1bfa8c6c497eb8557
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-10 13:45:56 +0300

    updating serverinfo

--- commands/say.js
+++ commands/say.js
@@ -7,12 +7,10 @@ module.exports = {
         const channel = client.channels.cache.get('860085472944848927')
 
         const user = message.mentions.users.first();
-    
-        if (!args[0]) {
-        message.channel.send("**Please Add Some Text for Me XOPBOT To Repeat! \nAnything Rude Is Sended To The Developer!**")
-        }
+
         let sayMessage = args.slice(0).join(' ');
         if (sayMessage.length > 65) return message.channel.send('**You Are Not Allowed To Go Over 65 Characters!**');
+
 
         if (message.content.match("gore") || message.content.match("fuck") || message.content.match("shit") || message.content.match("nigga") || message.content.match("hoe") || message.content.match("bitch") || message.content.match("dick") || message.content.match("gay") || message.content.match("lesbian") || message.content.match("blowjob") || message.content.match("porn") || message.content.match("cunt")) {
             message.delete();
@@ -20,11 +18,15 @@ module.exports = {
            // message.delete(4000);
             const embed = new Discord.MessageEmbed()
             .setTimestamp()
-            .setTitle(`Author \n${message.author.username} Used Say!`)
+            .setTitle(`Author \n${message.author.username} Used The Say Command!`)
             .setDescription(`Guild \nIn ${message.guild.name} \nHe Commanded This Word ${sayMessage}`)
             channel.send(embed);
         }
-
+    
+        if (!args[0]) {
+        message.channel.send("**Please Add Some Text for Me XOPBOT To Repeat! \nAnything Rude Is Sended To The Developer!**")
+        }
+ 
         message.channel.send(`${sayMessage}`, { allowedMentions: { parse:["users"] } });
     //  message.delete(10000);
 }
--- commands/serverinfo.js
+++ commands/serverinfo.js
@@ -19,28 +19,12 @@ module.exports = {
       "HIGH": "(╯°□°）╯︵  ┻━┻ (High)",
       "VERY_HIGH": "┻━┻ミヽ(ಠ益ಠ)ノ彡┻━┻ (Very High)"
   };
-  let region = {
-      "brazil": "Brazil",
-      "eu-central": "Central Europe",
-      "singapore": "Singapore",
-      "us-central": "U.S. Central",
-      "sydney": "Sydney",
-      "us-east": "U.S. East",
-      "us-south": "U.S. South",
-      "us-west": "U.S. West",
-      "eu-west": "Western Europe",
-      "vip-us-east": "VIP U.S. East",
-      "london": "London",
-      "amsterdam": "Amsterdam",
-      "hongkong": "Hong Kong",
-      "russia": "Russia",
-      "southafrica": "South Africa",
-      "india": "India"
-  };
+  const { guild } = message
+  const { region } = guild
 const serverembed = new Discord.MessageEmbed()
 .setAuthor(`${message.guild.name}`, message.guild.iconURL())
 .setThumbnail(servericon)
-.addField(`General Info`, `Owner: ${message.guild.owner} \nRegion: \`${region[message.guild.region]}\` \nVerification Lvl: \`${verifLevels[message.guild.verificationLevel]}\``)
+.addField(`General Info`, `Owner: ${message.guild.owner} \nRegion: \`${region}\` \nVerification Lvl: \`${verifLevels[message.guild.verificationLevel]}\``)
 .addField(`Overview`, `Total Channels: \`${message.guild.channels.cache.size}\` \nText Channels: \`${message.guild.channels.cache.filter((c) => c.type === "text").size}\` \nVoice Channels: \`${message.guild.channels.cache.filter((c) => c.type === "voice").size}\` \nAFK Channel: ${afk} \nAFK Timeout: \`${message.guild.afkTimeout} Sec\` \nTotal Roles: \`${message.guild.roles.cache.size}\` \nTotal Emojis: \`${message.guild.emojis.cache.size}\``)
 .addField(`Member Info`, `Total Members: \`${message.guild.memberCount}\` \nHumans: \`${message.guild.members.cache.filter(member => !member.user.bot).size}\` \nBots: \`${message.guild.members.cache.filter(member => member.user.bot).size}\``)
 .addField(`Misc. Info`, `You Joined on: \n\`${moment(mention.joinedAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\` \nCreated On: \n\`${moment(message.guild.createdAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\``)
