995c337dfb1ff9e6930f90ca643c65e7cecc41ee
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-21 15:57:20 +0300

    Update setbday.js

--- commands/setbday.js
+++ commands/setbday.js
@@ -1,5 +1,4 @@
 const bdaySchema = require('../models/bdayprofile');
-const moment = require('moment');
 
 module.exports = {
     name: 'setbday',
@@ -7,16 +6,18 @@ module.exports = {
     cooldown: 20,
     permissions: ["SEND_MESSAGES"],
     async execute(client, message, cmd, args, Discord) {
-
-        const doc = new bdaySchema({ _id: message.author.id });
-            if (!args[0]) {
-                return message.channel.send(`**${message.author.tag}**, Please Add The Date In DD-MM Format`);
-            };
-            const date = moment(date, 'DD-MM');
-            doc.data.profile.birthday = date.format('Do MMMM');
-
-            return doc.save()
-                .then(() => message.channel.send(`**${message.author.tag}**, Your Birthday Has Been Updated To **${date.format('Do MMMM')}**!`))
-                .catch(() => message.channel.send(`**${message.author.tag}**, Your Birthday Update Failed!`))
+        const data = new bdaySchema({ _id: message.author.id });
+        if (!args[0]) {
+            return message.channel.send(`**${message.author.tag}**, Please Add The Date In DD-MM Format`);
+        };
+        let bdayMessage = args.slice(0).join(' ');
+        if (bdayMessage.length > 5) return message.channel.send('**Your Bday Must Be Like Exa. 27-02 \nNot Longer Than 5 Characters Please!**');
+        const embed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setTitle('Bday Has Been Set Up')
+            .setDescription(`Your Bday Is ${bdayMessage}`)
+        data.save()
+            .then(() => message.channel.send(embed))
+            .catch(() => message.channel.send(`**${message.author.username} Failed To Set Your Bday! 😭`))
     }
 }
