a776b673b0ef5f3b11024daf939d8c1a01fd9d7d
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-21 19:24:44 +0300

    fixed sfw menu and added command

--- commands/help.js
+++ commands/help.js
@@ -295,7 +295,7 @@ module.exports = {
                 //.setTitle('__🔞 NSFW__')
                 .setColor('#c30202')
                 .addFields(
-                    { name: '__🔞 NSFW (23)__', value: 'Real Life NSFW: `4k` `neko` `pgif` `gonewild` `cumsluts` `blowjob` `dildo` `milf` `bdsm` `anal` `ass` `pussy` `boobs` `thigh` \nAnime NSFW: `hentai` `hthigh` `hanal` `hboobs` `paizuri` `yuri` `hmidriff` `hneko` `hkitsune` \nBot Invite: (https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295)' },
+                    { name: '__🔞 NSFW (24)__', value: '**Real Life NSFW**: `4k` `\nneko` `\npgif` `\ngonewild` `\ncreampie` `\ncumsluts` `\nblowjob` `\ndildo` `\nmilf` `\nbdsm` `\nanal` `\nass` `\npussy` `\nboobs` `\nthigh` \n**Anime NSFW**: `\nhentai` `\nhthigh` `\nhanal` `\nhboobs` `\npaizuri` `\nyuri` `\nhmidriff` `\nhneko` `\nhkitsune` \n**Bot Invite**: (https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295)' },
                 )
                 .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
 
