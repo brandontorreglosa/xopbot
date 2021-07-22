4e3427dff756ae7dd81236eee3fa7ebfb8668757
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-22 13:01:05 +0300

    sfw update

---
+++ commands/lewds.js
@@ -0,0 +1,34 @@
+const discord = require("discord.js");
+const got = require("got"); //MAKE SURE TO INSTALL THE PACKAGE "GOT" ELSE THE CODE WOULD NOT WORK
+
+
+module.exports = {
+      name: "lewds",
+      permissions: ["SEND_MESSAGES"],
+      category: "NSFW",
+      description: "Sends random lewds",
+      usage: "[command]",
+      async execute(client, message, cmd, args, Discord) {
+
+            if (!message.channel.nsfw) return message.channel.send('**This Is Not A NSFW Channel! 🔞**')
+
+            var lo = new Discord.MessageEmbed()
+                  .setDescription(`Sending Lewds...`)
+                  .setTimestamp()
+
+            message.channel.send(lo).then(m => {
+                  got('https://www.reddit.com/r/Lewd/random.json').then(response => {
+                        let content = JSON.parse(response.body);
+                        var title = content[0].data.children[0].data.title;
+                        var amazeme = content[0].data.children[0].data.url;
+                        let wow = new discord.MessageEmbed()
+                              .setTimestamp()
+                              .setDescription(`:underage: **Lewds**\n**[Provided To You By The Bot Supporters Of XOPBOT](${amazeme})**`)
+                              .setImage(amazeme)
+                              .setFooter(`Nice Yeah :)`)
+                              .setColor("RED")
+                        m.edit(wow)
+                  })
+            })
+      }
+}
