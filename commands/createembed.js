d9fd03f7dce48eea2e06a30b77da0f27ebdbe223
HACKERPROTM <brandontorreglosa@gmail.com>
2021-07-29 11:40:05 +0300

    Update createembed.js

--- commands/createembed.js
+++ commands/createembed.js
@@ -5,20 +5,19 @@ module.exports = {
     cooldown: 10,
     async execute(client, message, cmd, args, Discord) {
         if (!args[0]) {
-            return message.reply('**Please Do `x!createembed + <footermsg> + <title> + <color> + <description>`**')
+            return message.reply('**Please Do `x!createembed + <footermsg> + <title> + <description>`**')
         }
-        const footer = args.slice(0).join(" ");
+        let splitArgs = args.join(' ').split('/');
+        const footer = splitArgs[0];
         if (!footer) return message.reply('**Please Add `<footermsg>`**')
-        const title = args.slice(1).join(" ");
+        const title = splitArgs[1];
         if (!title) return message.reply('**Please Add `<title>`**')
-        const color = args.slice(2).join(" ");
-        if (!color) return message.reply('**Please Add `<color> Example. `RED` `BLUE` `GREEN` Etc. \nMust Be All Capitals To Work For Embed!`**')
-        const description = args.slice(3).join(" ");
+        const description = splitArgs[2];
         if (!description) return message.reply('**Please Add `<description>`**')
-
+    
         const embed = new Discord.MessageEmbed()
             .setTimestamp()
-            .setColor(`${color}`)
+            .setColor(`#c30202`)
             .setTitle(`${title}`)
             .setDescription(`${description}`)
             .setFooter(`${footer}`)
