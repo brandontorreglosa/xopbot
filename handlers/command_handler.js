d0edea0dd88202258244484dec02ad02af5ff654
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 09:26:35 +0300

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -2,7 +2,7 @@ const fs = require('fs');
 
 module.exports = (client, Discord) => {
 
-    const command_folders = readdirSync("./commands")
+    const command_folders = fs.readdirSync("./commands")
 
     for (const folder of command_folders) {
         const command_files = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
