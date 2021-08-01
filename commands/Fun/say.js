3575766160a83a8a531c3e4f5c252769b99f6b85
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 19:53:34 +0300

    work pls

--- commands/Fun/say.js
+++ commands/Fun/say.js
@@ -13,6 +13,10 @@ module.exports = {
 
             const user = message.mentions.users.first();
 
+            if (!args[0]) {
+                message.channel.send("**Please Add Some Text For Me XOPBOT To Repeat!**")
+            }
+
             if (message.content.match("gore") || message.content.match("boob") || message.content.match("tits") || message.content.match("fuck") || message.content.match("shit") || message.content.match("nigga") || message.content.match("hoe") || message.content.match("bitch") || message.content.match("dick") || message.content.match("gay") || message.content.match("lesbian") || message.content.match("blowjob") || message.content.match("porn") || message.content.match("cunt")) {
                 message.delete();
                 message.reply(`**Im Not Allowed To Say Bad Words! Lol Cry 😂**`);
@@ -26,10 +30,6 @@ module.exports = {
 
             let sayMessage = args.slice(0).join(' ');
             if (sayMessage.length > 100) return message.channel.send('**You Are Not Allowed To Go Over 100 Characters!**');
-
-            if (!args[0]) {
-                message.channel.send("**Please Add Some Text For Me XOPBOT To Repeat!**")
-            }
 
             const embed = new Discord.MessageEmbed()
                 .setTimestamp()
