d55e5ea212e065064948166c9896b577b89936dc
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 10:32:15 +0300

    Update batmanslap.js

--- commands/Images/batmanslap.js
+++ commands/Images/batmanslap.js
@@ -9,11 +9,13 @@ module.exports = {
     async execute(client, message, cmd, args, Discord) {
 
         if(!args[0]) {
-            return message.reply('**`(preifx)batmanslap text1 / text2` \nMust Add / For It To Work!**')
+            return message.reply('**`(preifx)batmanslap @user{optional} text1 / text2` \nMust Add / For It To Work!**')
         }
 
         const mention = message.mentions.members.first() || message.member;
         const avatar = mention.user.displayAvatarURL({ size: 2048, format: "png" });
+        const mention2 = message.mentions.members.first() || message.member;
+        const avatar2 = mention.user.displayAvatarURL({ size: 2048, format: "png" });
 
         let splitargs = args.join(' ').split('/');
 
@@ -29,6 +31,6 @@ module.exports = {
         }
         if (text2.length > 50) return message.reply(`**You Cant Go Over 50 Characters!**`)
 
-        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/batmanslap?text1=TEXT&text2=TEXT&batman=${avatar}&robin=https://cdn.discordapp.com/avatars/831824859066925087/be9c11f1817e227ac146cbacd0660aac.webp`, name: "xopbotdockofshame.png" }] });
+        message.channel.send({ files: [{ attachment: `https://vacefron.nl/api/batmanslap?text1=${text}&text2=${text2}&batman=${avatar}&robin=https://cdn.discordapp.com/avatars/831824859066925087/be9c11f1817e227ac146cbacd0660aac.webp`, name: "xopbotdockofshame.png" }] });
     }
 }
