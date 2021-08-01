41ce662f01edb41efddd91201b7de4ea9512f983
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 20:38:07 +0300

    update sfw

--- commands/NSFW/orgy.js
+++ commands/NSFW/orgy.js
@@ -3,31 +3,31 @@ const got = require("got"); //MAKE SURE TO INSTALL THE PACKAGE "GOT" ELSE THE CO
 
 
 module.exports = {
-      name: "cowgirl",
+      name: "orgy",
       nsfw: true,
       cooldown: 3,
       permissions: ["SEND_MESSAGES"],
       category: "NSFW",
-      description: "Sends random cowgirls",
+      description: "Sends random succubus",
       usage: "[command]",
       async execute(client, message, cmd, args, Discord) {
 
             if (!message.channel.nsfw) return message.channel.send('**This Is Not A NSFW Channel! 🔞**')
 
             var lo = new Discord.MessageEmbed()
-                  .setDescription(`Sending Cowgirl...`)
+                  .setDescription(`Sending Orgy...`)
                   .setTimestamp()
 
             message.channel.send(lo).then(m => {
-                  got('https://www.reddit.com/r/Cowgirls/random.json').then(response => {
+                  got('https://www.reddit.com/r/orgy/random.json').then(response => {
                         let content = JSON.parse(response.body);
                         var title = content[0].data.children[0].data.title;
                         var amazeme = content[0].data.children[0].data.url;
                         let wow = new discord.MessageEmbed()
                               .setTimestamp()
-                              .setDescription(`:underage: **Cowgirl**\n**[Provided To You By The Bot Supporters Of XOPBOT](${amazeme})**`)
+                              .setDescription(`:underage: **Orgy**\n**[Provided To You By The Bot Supporters Of XOPBOT](${amazeme})**`)
                               .setImage(amazeme)
-                              .setFooter(`Moo! Can I Have Some Milk! :)`)
+                              .setFooter(`So Many On Top Of Many! :)`)
                               .setColor('#c30202')
                         m.edit(wow)
                   })
