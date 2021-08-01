822ef299e7748b19f30bcfd4db25680bda60272a
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 09:15:59 +0300

    Update command_handler.js

--- handlers/command_handler.js
+++ handlers/command_handler.js
@@ -1,14 +1,16 @@
 const fs = require('fs');
 
 module.exports = (client, Discord) => {
-    const command_files = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
+    for (const folder of command_folders) {
+        const command_files = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'));
 
-    for (const file of command_files) {
-        const command = require(`../commands/${folder}/${file}`);
-        if (command.name) {
-            client.commands.set(command.name, command);
-        } else {
-            continue;
+        for (const file of command_files) {
+            const command = require(`../commands/${folder}/${file}`);
+            if (command.name) {
+                client.commands.set(command.name, command);
+            } else {
+                continue;
+            }
         }
     }
 }
