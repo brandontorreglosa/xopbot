cd611714e94ab4cb1f299b79c2b58f91472c931a
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-01 09:37:46 +0300

    update finished

--- counters/member-counter.js
+++
@@ -1,9 +0,0 @@
-// module.exports = async (client) =>{
-//     const guild = client.guilds.cache.get('707557478254247936');
-//     setInterval(() =>{
-//         const memberCount = guild.memberCount;
-//         const channel = guild.channels.cache.get('828322456992743446');
-//         channel.setName(`Total Members: ${memberCount.toLocaleString()}`);
-//         console.log('Updating Member Count');
-//     }, 5000);
-// }
