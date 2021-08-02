215ca8d30afd2a81f5776185bd37305c85e5b2f3
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-02 15:17:01 +0300

    Create rickroll.js

---
+++ commands/Images/rickroll.js
@@ -0,0 +1,16 @@
+module.exports = {
+    name: 'rickroll',
+    permissions: ["SEND_MESSAGES"],
+    cooldown: 5,
+    description: 'get Rickrolled',
+    async execute(client, message, cmd, args, Discord) {
+        const user = message.mentions.users.first() || message.author;
+        const embed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setColor('#c30202')
+            .setTitle(`OMG! You Just Got Rickrolled By The Legendary ${message.author.username}`)
+            .setImage('')
+            .setFooter('You Just Got Rickrolled 😆')
+            user.send(embed)
+    }
+}
