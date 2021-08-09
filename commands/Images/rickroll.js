6d111b8dc031d713a86b8f8c14368a01a17d63d8
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-09 13:45:27 +0300

    Update rickroll.js

--- commands/Images/rickroll.js
+++ commands/Images/rickroll.js
@@ -11,6 +11,8 @@ module.exports = {
             .setTitle(`OMG! You Just Got Rickrolled By ${message.author.username}`)
             .setImage('https://media1.tenor.com/images/23aeaaa34afd591deee6c163c96cb0ee/tenor.gif?itemid=7220603')
             .setFooter('You Just Got Rickrolled 😆')
-            user.send(embed)
+        user.send(embed)
+            .catch(() => message.channel.send("**That user could not be DMed!**"))
+            .then(() => message.channel.send(`**Sent a message to ${user.user.tag}**`));
     }
 }
