4d7becbd4a33706d5528e0615fc044ecb288c8a4
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-10 18:08:11 +0300

    update images

---
+++ commands/Images/uncover.js
@@ -0,0 +1,16 @@
+const Discord = require("discord.js");
+
+module.exports = {
+    name: "uncover",
+    permissions: ["SEND_MESSAGES"],
+    cooldown: 3,
+    description: "Image Manipulation Command",
+    async execute(client, message, cmd, args, Discord) {
+
+        const mention = message.mentions.members.first() || message.member;
+        const avatar = mention.user.displayAvatarURL({ size: 2048, format: "png" });
+
+
+        message.channel.send({ files: [{ attachment: `https://api.popcatdev.repl.co/uncover?image=${avatar}`, name: "xopbotrickandmortyuncover.png" }] });
+    }
+}
