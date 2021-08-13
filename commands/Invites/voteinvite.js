4d3834e88698a11d68656b0321dfbba48d6f3f65
Brandon Torreglosa <84269666+HACKERPROTM@users.noreply.github.com>
2021-08-13 15:14:40 +0300

    Create voteinvite.js

---
+++ commands/Invites/voteinvite.js
@@ -0,0 +1,25 @@
+module.exports = {
+    name: 'voteinvite',
+    cooldown: 3,
+    permissions: ["SEND_MESSAGES"],
+    aliases: ['vinv', 'voin'],
+    description: "Embeds!",
+    execute(client, message, cmd, args, Discord) {
+        const newEmbed = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setThumbnail(message.author.displayAvatarURL({ dynamic: true }))
+            .setColor('#c30202')
+            .setTitle('Vote Invite')
+            .setURL('https://top.gg/bot/831824859066925087')
+            .setDescription('**Love XOPBOT? 😍 Well Then! \nYou Can Vote Down Here ⤵**')
+            .addFields(
+                { name: 'Vote Invite Link', value: '[Bot Website](https://top.gg/bot/831824859066925087)' }
+
+            )
+            .setFooter(`Requested By: ${message.author.tag}`, message.author.displayAvatarURL())
+
+
+        message.channel.send(newEmbed);
+    }
+
+}
