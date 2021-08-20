bcdd61b7fdf3825f74463dbea99c455a716d5aa0
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-20 14:22:12 +0300

    yep now works!

--- index.js
+++ index.js
@@ -25,40 +25,6 @@ const DiscordButtons = require('discord-buttons')
 DiscordButtons(client);
 const fs = require('fs')
 
-//<----/Button Commands/---->
-client.on('clickButton', (button) => {
-    ButtonPages.buttonInteractions(button, client.interaction);
-});
-
-client.on('message', msg => {
-    if (message.content.match('config')) {
-        message.react('🌘');
-
-        const embed15 = new Discord.MessageEmbed()
-            .setThumbnail(client.user.displayAvatarURL())
-            .setTimestamp()
-            //.setTitle('__🎮 Games__')
-            .setColor('#c30202')
-            .addFields(
-                { name: '__⚠ Config (9)__', value: '\n[antiwords](https://xopbot-gg.glitch.me/) \n__***Setup Bad Words Detector!***__ \n[antilink](https://xopbot-gg.glitch.me/) \n__***Setup Antilink On A Server!***__ \n[autonsfw](https://xopbot-gg.glitch.me/) \n__***Setup Autonsfw On A Server!***__ \n[automeme](https://xopbot-gg.glitch.me/) \n__***Setup Automeme On A Server!***__ \n[joinchannel](https://xopbot-gg.glitch.me/) \n__***Setup The Join Channel!***__' }
-            )
-            .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
-
-        const embed16 = new Discord.MessageEmbed()
-            .setThumbnail(client.user.displayAvatarURL())
-            .setTimestamp()
-            //.setTitle('__🎮 Games__')
-            .setColor('#c30202')
-            .addFields(
-                { name: '__⚠ Config (9)__', value: '\n[leavechannel](https://xopbot-gg.glitch.me/) \n__***Setup The Leave Channel!***__ \n\[joinmessage](https://xopbot-gg.glitch.me/) \n__***Setup The Join Message!***__ \n\[leavemessage](https://xopbot-gg.glitch.me/) \n__***Setup The Leave Message!***__ \n[setprefix](https://xopbot-gg.glitch.me/) \n__***Set The Server`s Custom Prefix!***__' }
-            )
-            .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
-
-        const embedPages = [embed15, embed16]
-        ButtonPages.createPages(client.interaction, msg, embedPages, 60 * 1000, "blurple", "⏩", "⏪", "❌");
-    }
-});
-
 // <----/Client Events/---->
 
 client.db = new Database(process.env.MONGODB_SRV2)
