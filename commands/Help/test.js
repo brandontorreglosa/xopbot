646b4521c15fe0437d4df285f9e7b3bc6ae8543a
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-21 14:04:05 +0300

    Create test.js

---
+++ commands/Help/test.js
@@ -0,0 +1,29 @@
+const { pagination } = require("reconlx");
+module.exports = {
+    name: 'test',
+    permissions: ["SEND_MESSAGES"],
+    async execute(client, message, cmd, args, Discord) {
+        const embed = new Discord.MessageEmbed()
+            .setTitle('Boi')
+        const embed2 = new Discord.MessageEmbed()
+            .setTitle('Boi')
+        const embed3 = new Discord.MessageEmbed()
+            .setTitle('Boi')
+        const embed4 = new Discord.MessageEmbed()
+            .setTitle('Boi')
+
+        const embeds = [
+            embed,
+            embed2,
+            embed3,
+            embed4,
+        ];
+
+        pagination({
+            embeds: embeds,
+            message: message,
+            time: 120000,
+            fastSkip: true,
+        })
+    }
+}
