0efc45fa09abd4f4a14ed2cdaeca88b401f3bea8
HACKERPROTM <brandontorreglosa@gmail.com>
2021-08-29 20:44:45 +0300

    Update phub.js

--- commands/Images/phub.js
+++ commands/Images/phub.js
@@ -7,7 +7,7 @@ module.exports = {
     description: 'what are you doing here?',
     async execute(client, message, cmd, args, Discord) {
         if (!args[0]) {
-            return message.lineReplyNoMention({ content: '**`(prefix)phub <text> / <text2>` \nYou Must Add The / For It Work!**' }) //, allowedMentions: { repliedUser: true } })
+            return message.lineReplyNoMention({ content: '**`(prefix)phub <text>`**' }) //, allowedMentions: { repliedUser: true } })
         }
         let splitArgs = args.join(' ').split('/');
         const text = splitArgs[0];
@@ -16,15 +16,9 @@ module.exports = {
         }
         if (text.length > 70) return message.reply({ content: `**You Cant Go Over 70 Characters!**`, allowedMentions: { repliedUser: true } })
 
-        const text2 = splitArgs[1];
-        if (!text2) {
-            return message.lineReplyNoMention({ content: "**Enter The Second Text!**" }) //, allowedMentions: { repliedUser: true } })
-        }
-        if (text2.length > 70) return message.lineReplyNoMention({ content: `**You Cant Go Over 70 Characters!**` }) //, allowedMentions: { repliedUser: true }})
-
         const image = await Canvas.phub({
             username: member,
-            message: bidenMessage,
+            message: text,
             image: avatar,
         });
 
