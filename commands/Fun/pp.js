01042c2ccbc4965f9b610759abc551b9d7e3dd6a
HACKERPROTM <brandontorreglosa@gmail.com>
2021-09-17 21:42:39 +0300

    Update pp.js

--- commands/Fun/pp.js
+++ commands/Fun/pp.js
@@ -40,14 +40,14 @@ module.exports = {
             '8============================D'
         ];
 
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.member;
+        const user = message.mentions.users.first() || message.member;
 
         if (!user)
             return message.lineReplyNoMention({ content: `**Mention Someone Or Provide Their User ID To Get Their PP Size!**` }) //, allowedMentions: { repliedUser: true } })
 
         const embed = new Discord.MessageEmbed()
             .setTitle('PP Size Detector')
-            .setDescription(`\`${user.user.username}\`'s PP Is This Big\n\`\`\`${pp[Math.floor(Math.random() * pp.length)]}\`\`\``)
+            .setDescription(`\`${user.username}\`'s PP Is This Big\n\`\`\`${pp[Math.floor(Math.random() * pp.length)]}\`\`\``)
             .setFooter(`Requested By ${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
             .setTimestamp()
             .setColor(`${color}`);
