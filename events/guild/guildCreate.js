7553bfe2e509ed9e162ec4b207644d330d8482b9
HACKERPROTM <brandontorreglosa@gmail.com>
2021-09-19 00:20:20 +0300

    updaye

--- events/guild/guildCreate.js
+++ events/guild/guildCreate.js
@@ -1,43 +1,43 @@
-const Discord = require('discord.js')
-const lineReplyNoMention = require('discord-reply');
-const { MessageButton } = require('discord-buttons');
-const color = process.env.Color;
-// const xopemoji = client.emojis.get(888783517012594729);
-const client = require('../../index')
-client.on("guildCreate", (guild) => {
+// const Discord = require('discord.js')
+// const lineReplyNoMention = require('discord-reply');
+// const { MessageButton } = require('discord-buttons');
+// const color = process.env.Color;
+// // const xopemoji = client.emojis.get(888783517012594729);
+// const client = require('../../index')
+// client.on("guildCreate", (guild) => {
 
-    let channelToSendTo;
+//     let channelToSendTo;
 
-    guild.channels.cache.forEach((channel) => {
-        if (
-            channel.type === 'text' &&
-            !channelToSendTo &&
-            channel.permissionsFor(guild.me).has('SEND_MESSAGES')
-        )
-            channelToSendTo = channel;
-    });
+//     guild.channels.cache.forEach((channel) => {
+//         if (
+//             channel.type === 'text' &&
+//             !channelToSendTo &&
+//             channel.permissionsFor(guild.me).has('SEND_MESSAGES')
+//         )
+//             channelToSendTo = channel;
+//     });
 
-    if (!channelToSendTo);
+//     if (!channelToSendTo);
 
-    const button11 = new MessageButton()
-        .setStyle('url')
-        .setURL('https://xopbot-gg.glitch.me/')
-        .setLabel('Website')
-        .setEmoji(`💻`)
+//     const button11 = new MessageButton()
+//         .setStyle('url')
+//         .setURL('https://xopbot-gg.glitch.me/')
+//         .setLabel('Website')
+//         .setEmoji(`💻`)
 
-    const button12 = new MessageButton()
-        .setStyle('url')
-        .setURL('https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295')
-        .setLabel('Invite')
-        .setEmoji(`🔗`)
+//     const button12 = new MessageButton()
+//         .setStyle('url')
+//         .setURL('https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295')
+//         .setLabel('Invite')
+//         .setEmoji(`🔗`)
 
-    const row3 = new MessageActionRow()
-        .addComponents(button11, button12)
+//     const row3 = new MessageActionRow()
+//         .addComponents(button11, button12)
 
-    const embed = new MessageEmbed()
-        .setTimestamp()
-        .setColor(`${color}`)
-        .setAuthor(`You Added Me In ${message.guild.name}!`, message.guild.iconURL())
-        .setDescription(`**Thank You For Inviting Me To \`${message.guild.name}\` \nName Is \`XOPBOT\` Im A Multipurpose Bot With Over 200+ Commands And \`24/7\` Uptime \nMy Prefix Is \`x!\` And You Can Get My Commands By Doing \`x!help\` \nYou Can Also Check Out My Websites Down Below⤵**`)
-    channelToSendTo.send(embed, row3)
-})
+//     const embed = new MessageEmbed()
+//         .setTimestamp()
+//         .setColor(`${color}`)
+//         .setAuthor(`You Added Me In ${message.guild.name}!`, message.guild.iconURL())
+//         .setDescription(`**Thank You For Inviting Me To \`${message.guild.name}\` \nName Is \`XOPBOT\` Im A Multipurpose Bot With Over 200+ Commands And \`24/7\` Uptime \nMy Prefix Is \`x!\` And You Can Get My Commands By Doing \`x!help\` \nYou Can Also Check Out My Websites Down Below⤵**`)
+//     channelToSendTo.send(embed, row3)
+// })
--- index.js
+++ index.js
@@ -206,7 +206,7 @@ client.distube
             .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
             .setDescription(`**Channel Is \`Empty\`. XOPBOT Leaving The Channel In \`2m\`! 😭**`)
 
-            const leftdavcc = new Discord.MessageEmbed()
+        const leftdavcc = new Discord.MessageEmbed()
             .setTimestamp()
             .setColor(`${color}`)
             .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
@@ -284,4 +284,42 @@ client.distube
         message.lineReplyNoMention(embed);
     });
 
+//<----/Join Guild System/---->
+client.on("guildCreate", (guild) => {
+
+    let channelToSendTo;
+
+    guild.channels.cache.forEach((channel) => {
+        if (
+            channel.type === 'text' &&
+            !channelToSendTo &&
+            channel.permissionsFor(guild.me).has('SEND_MESSAGES')
+        )
+            channelToSendTo = channel;
+    });
+
+    if (!channelToSendTo);
+
+    const button11 = new MessageButton()
+        .setStyle('url')
+        .setURL('https://xopbot-gg.glitch.me/')
+        .setLabel('Website')
+        .setEmoji(`💻`)
+
+    const button12 = new MessageButton()
+        .setStyle('url')
+        .setURL('https://discord.com/oauth2/authorize?client_id=831824859066925087&scope=bot&permissions=4294967295')
+        .setLabel('Invite')
+        .setEmoji(`🔗`)
+
+    const row3 = new MessageActionRow()
+        .addComponents(button11, button12)
+
+    const embed = new MessageEmbed()
+        .setTimestamp()
+        .setColor(`${color}`)
+        .setAuthor(`You Added Me In ${message.guild.name}!`, message.guild.iconURL())
+        .setDescription(`**Thank You For Inviting Me To \`${message.guild.name}\` \nName Is \`XOPBOT\` Im A Multipurpose Bot With Over 200+ Commands And \`24/7\` Uptime \nMy Prefix Is \`x!\` And You Can Get My Commands By Doing \`x!help\` \nYou Can Also Check Out My Websites Down Below⤵**`)
+    channelToSendTo.send(embed, row3)
+})
 client.login(process.env.DISCORD_TOKEN);
