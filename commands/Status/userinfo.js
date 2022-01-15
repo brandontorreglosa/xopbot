0a47f56e1ad38b51ae69d6cbccdaefb55527abfd
HACKERPROTM <brandontorreglosa@gmail.com>
2022-01-15 12:11:40 +0200

    Update userinfo.js

--- commands/Status/userinfo.js
+++ commands/Status/userinfo.js
@@ -8,7 +8,7 @@ module.exports = {
     aliases: ['usi', 'ui'],
     cooldown: 3,
     async execute(client, message, cmd, args, Discord) {
-        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.author;
+        const user = message.mentions.users.first() || message.guild.members.cache.get(args[0]) || message.member;
 
         let status;
         switch (user.presence.status) {
@@ -28,9 +28,9 @@ module.exports = {
 
         const embed = new Discord.MessageEmbed()
             .setTimestamp()
-            .setAuthor(`${user.user.username}\`s User Information`, user.user.displayAvatarURL({ dynamic: true }))
+            .setAuthor(`${user.username}\`s User Information`, user.displayAvatarURL({ dynamic: true }))
             .setColor(`${color}`)
-            .setDescription(`**ID:** \`${user.id}\` \n**Name:** \`${user.user.username}\` \n**Discriminator:** \`#${user.user.discriminator}\` \n**User Status:** \`${status}\` \n**User Roles:** ${user.roles.cache.map(role => role.toString()).join(" ,")} \n**Joined Date:** \`${moment(user.joinedAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\` \n**Creation Date:** \`${moment(user.createdAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\` `)
+            .setDescription(`**ID:** \`${user.id}\` \n**Name:** \`${user.username}\` \n**Discriminator:** \`#${user.discriminator}\` \n**User Status:** \`${status}\` \n**User Roles:** ${user.roles.cache.map(role => role.toString()).join(" ,")} \n**Joined Date:** \`${moment(user.joinedAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\` \n**Creation Date:** \`${moment(user.createdAt).format("dddd, MMMM Do YYYY, h:mm:ss A")}\` `)
             .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
         await message.lineReplyNoMention(embed)
     }
