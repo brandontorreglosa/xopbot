369d4241c58ade5f95904edc71867f4381d8b535
HACKERPROTM <brandontorreglosa@gmail.com>
2022-02-01 17:45:00 +0200

    `update economy`

---
+++ commands/Economy/buy.js
@@ -0,0 +1,73 @@
+const inventory = require("../../models/inventory");
+const items = require("../../models/shopitems");
+const color = process.env.Color;
+const lineReplyNoMention = require("discord-reply");
+
+module.exports = {
+    name: "buy",
+    permissions: ["SEND_MESSAGES"],
+    clientpermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    cooldown: 10,
+    description: "buy something",
+    async execute(client, message, cmd, args, Discord) {
+        if (!args[0]) {
+            const nospec = new Discord.MessageEmbed()
+                .setTimestamp()
+                .setColor(`${color}`)
+                .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setDescription('**`(prefix)buy <item-name>`**')
+            return message.lineReplyNoMention(nospec)
+        }
+
+        const itemToBuy = args[0].toLowerCase()
+
+        const validItem = !!items.find((val) => val.item.toLowerCase() === itemToBuy);
+
+        if (!validItem) {
+            const novalid = new Discord.MessageEmbed()
+                .setTimestamp()
+                .setColor(`${color}`)
+                .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+                .setDescription('**The Item Mentioned Does Not Exist Or Is Not Valid!**')
+            return messsage.lineReplyNoMention(novalid)
+        }
+
+        const itemPrice = items.find((val) => (val.item.toLowerCase()) === itemToBuy).price;
+        const userBalance = await client.bal(message.author.id);
+
+        const noxocoins = new Discord.MessageEmbed()
+            .setTimestamp()
+            .setColor(`${color}`)
+            .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+            .setDescription(`**You Only Have \`${userBalance}\` Xocoins! You Need \`${itemPrice}\` Xocoins To Buy It!**`)
+
+        if (userBalance < itemPrice) return message.lineReplyNoMention(noxocoins);
+
+        const params = {
+            Guild: message.guild.id,
+            User: message.author.id,
+        };
+        inventory.findOne(params, async (err, data) => {
+            if (data) {
+                const hasItem = Object.keys(data.Inventory).includes(itemToBuy);
+                if (!hasItem) {
+                    data.Inventory[itemToBuy] = 1;
+                } else {
+                    data.Inventory[itemToBuy]++;
+                }
+                console.log(data);
+                await inventory.findOneAndUpdate(params, data);
+            } else {
+                new inventory({
+                    Guild: message.guild.id,
+                    User: message.author.id,
+                    Inventory: {
+                        [itemToBuy]: 1,
+                    },
+                }).save();
+            }
+            message.reply({content: `**You Have Bought The Item \`${itemToBuy}\` For \`${itemPrice}\` Xocoins! The Shop Has Taken That Price!**`});
+            client.rmv(message.author.id, itemPrice);
+        })
+    }
+}
---
+++ models/shopitems.js
@@ -0,0 +1,42 @@
+module.exports = [
+    {
+        item: "🖥️ Computer",
+        price: 4000,
+    },
+    {
+        item: "🎣 Fishing Rod",
+        price: 6000,
+    },
+    {
+        item: "🪓 Battle Axe",
+        price: 8000,
+    },
+    {
+        item: "🔫 Hunting Gun",
+        price: 15000,
+    },
+    {
+        item: "🏹 Crossbow",
+        price: 22000,
+    },
+    {
+        item: "😺 Cat",
+        price: 30000,
+    },
+    {
+        item: "🐶 Dog",
+        price: 38000,
+    }, 
+    {
+        item: "🤖 Robot",
+        price: 45000,
+    },
+    {
+        item: "🐸 Dank Frog",
+        price: 80000,
+    },
+    {
+        item: "<:FBI_OPEN_UP:824886936862457897> FBI Patrick",
+        price: 120000,
+    }
+];
