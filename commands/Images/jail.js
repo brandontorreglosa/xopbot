f005888236b8a56e734b2180ae04363b34a1ec83
HACKERPROTM <brandontorreglosa@gmail.com>
2022-02-26 15:10:39 +0200

    `update images v3`

--- commands/Images/jail.js
+++ commands/Images/jail.js
@@ -2,16 +2,16 @@ const lineReplyNoMention = require("discord-reply");
 const color = process.env.Color;
 const DIG = require("discord-image-generation");
 module.exports = {
-    name: "blur",
+    name: "jail",
     cooldown: 10,
     permissions: ["SEND_MESSAGES"],
     clientpermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-    description: "just blur a photo",
+    description: "you went to jail, stop doing bad stuff you're only 13!",
     async execute(client, message, cmd, args, Discord) {
         const user = message.mentions.users.first() || message.author;
         const avatar = user.displayAvatarURL({ dynamic: true, format: 'png' });
-        const img = await new DIG.Blur().getImage(`${avatar}`, 2);
-        const attachment = new Discord.MessageAttachment(img, "xopbotblur.png")
+        const img = await new DIG.Jail().getImage(`${avatar}`);
+        const attachment = new Discord.MessageAttachment(img, "xopbotjail.png")
         return message.lineReplyNoMention({ files: [attachment] });
     }
 }
