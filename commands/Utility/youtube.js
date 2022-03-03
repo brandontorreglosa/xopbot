146072c52ca944ec3f7ca2d8486fa8111130c32b
HACKERPROTM <brandontorreglosa@gmail.com>
2022-03-03 07:37:15 +0200

    `update utility commands v2`

--- commands/Utility/youtube.js
+++ commands/Utility/youtube.js
@@ -16,26 +16,22 @@ module.exports = {
         .setDescription(`**\`(prefix)youtube <video name>\`**`)
       return message.lineReplyNoMention(nopr)
     }
-   
+
     const args = message.content.split(" ").slice(1)
 
     const videoFinder = async (query) => {
-             const videoResult = await ytSearch(query);
-  
-             return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
-  
-         }
-  
-         const video = await videoFinder(args.join(' '));
- 
-         if(video) {
- 
-         
-            message.lineReplyNoMention(`${video.url}`)
-       
-   
-   } else {
-    message.lineReplyNoMention('no video found. sad')
-   }
-   }
+      const videoResult = await ytSearch(query);
+
+      return (videoResult.videos.length > 1) ? videoResult.videos[0] : null;
+
+    }
+
+    const video = await videoFinder(args.join(' '));
+
+    if (video) {
+      message.lineReplyNoMention(`${video.url}`)
+    } else {
+      message.lineReplyNoMention('no video found. sad')
+    }
+  }
 }
