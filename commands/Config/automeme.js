5eefe3bc79244248a914aec3e29b040b0d740c9e
HACKERPROTM <brandontorreglosa@gmail.com>
2022-03-03 17:09:25 +0200

    `update economy cmds`

--- commands/Config/automeme.js
+++ commands/Config/automeme.js
@@ -1,6 +1,7 @@
 const got = require('got');
 const lineReplyNoMention = require('discord-reply');
 const color = process.env.Color;
+const { MessageButton, MessageActionRow } = require("discord-buttons");
 module.exports = {
     name: "automeme",
     cooldown: 300,
@@ -25,56 +26,68 @@ module.exports = {
             .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
             .setDescription('✅ **| AutoMeme Started**')
 
+        const button1 = new MessageButton()
+            .setStyle('green')
+            .setID('accept')
+            .setLabel("Accept")
+            .setEmoji('✅')
+
+        const button2 = new MessageButton()
+            .setStyle('red')
+            .setID('reject')
+            .setLabel('Reject')
+            .setEmoji('❌')
+
+        const row = new MessageActionRow()
+            .addComponents(button1, button2)
+
         const stopvote = new Discord.MessageEmbed()
             .setTimestamp()
             .setColor(`${color}`)
             .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
             .setDescription(`**This Year Currently On Reddit: \nThere Has Been \`Dirty Users\` On \`SFW Subreddits\` And Spam \`NSFW Content\` \nIf U Dont Want NSFW Please Dont Execute This! \nDo U Still Agree To Continue?**`)
             .setFooter('Please Reply With Yes Or No!')
-        message.lineReplyNoMention(stopvote)
+        message.lineReplyNoMention(stopvote, row)
 
-        const filter = _message => message.author.id === _message.author.id && ['y', 'n', 'yes', 'no'].includes(_message.content.toLowerCase());
-        const options = { max: 1, time: 30000, errors: ['time'] };
-        const proceed = await message.channel.awaitMessages(filter, options)
-            .then(collected => ['y', 'yes'].includes(collected.first().content.toLowerCase()) ? true : false)
-            .catch(() => false);
-
-        if (!proceed) {
-            const nostopcmdplz = new Discord.MessageEmbed()
-                .setTimestamp()
-                .setColor(`${color}`)
-                .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-                .setDescription(`**You Cancelled The Automeme Command Successfully!**`)
-            return message.lineReplyNoMention(nostopcmdplz)
-        };
-
-        await message.lineReplyNoMention(on1).then((msg) => {
-            setTimeout(function () {
-                msg.edit(on2)
-                setTimeout(function () {
-                    msg.edit(on3)
-                }, 10000)
-            }, 10000)
-        })
-        setInterval(() => {
-            got('https://www.reddit.com/r/memes/random.json').then(response => {
-                let content = JSON.parse(response.body);
-                let permalink = content[0].data.children[0].data.permalink;
-                let memeUrl = `https://reddit.com${permalink}`;
-                let memeImage = content[0].data.children[0].data.url;
-                let memeTitle = content[0].data.children[0].data.title;
-                let memeUpvotes = content[0].data.children[0].data.ups;
-                let memeDownvotes = content[0].data.children[0].data.downs;
-                let memeNumComments = content[0].data.children[0].data.num_comments;
-                const embed = new Discord.MessageEmbed()
-                embed.setTimestamp()
-                embed.setTitle(`${memeTitle}`)
-                embed.setURL(`${memeUrl}`)
-                embed.setImage(`${memeImage}`)
-                embed.setColor(`${color}`)
-                embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
-                message.lineReplyNoMention(embed);
-            })
-        }, 20000)
+        client.on("clickButton", async (button) => {
+            if (button.id === "reject") {
+                const nostopcmdplz = new Discord.MessageEmbed()
+                    .setTimestamp()
+                    .setColor(`${color}`)
+                    .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+                    .setDescription(`**You Cancelled The Automeme Command Successfully!**`)
+                return message.lineReplyNoMention(nostopcmdplz)
+            } else if (button.id === "accept") {
+                message.lineReplyNoMention(on1).then((msg) => {
+                    setTimeout(function () {
+                        msg.edit(on2)
+                        setTimeout(function () {
+                            msg.edit(on3)
+                        }, 10000)
+                    }, 10000)
+                })
+                setInterval(() => {
+                    got(`https://www.reddit.com/r/memes/random.json`).then(response => {
+                        let content = JSON.parse(response.body);
+                        let permalink = content[0].data.children[0].data.permalink;
+                        let memeUrl = `https://reddit.com${permalink}`;
+                        let memeImage = content[0].data.children[0].data.url;
+                        let memeTitle = content[0].data.children[0].data.title;
+                        let memeUpvotes = content[0].data.children[0].data.ups;
+                        let memeDownvotes = content[0].data.children[0].data.downs;
+                        let memeNumComments = content[0].data.children[0].data.num_comments;
+                        const embed = new Discord.MessageEmbed()
+                        embed.setTimestamp()
+                        embed.setTitle(`${memeTitle}`)
+                        embed.setURL(`${memeUrl}`)
+                        embed.setImage(`${memeImage}`)
+                        embed.setColor(`${color}`)
+                        embed.setFooter(`👍 ${memeUpvotes} 👎 ${memeDownvotes} 💬 ${memeNumComments}`)
+                        message.lineReplyNoMention(embed);
+                    })
+                }, 20000)
+            }
+            button.reply.defer();
+        });
     }
 }
--- commands/Economy/beg.js
+++ commands/Economy/beg.js
@@ -1,6 +1,7 @@
 const lineReplyNoMention = require('discord-reply');
 const color = process.env.Color;
 const db = require("quick.db");
+const { MessageButton, MessageActionRow } = require("discord-buttons");
 module.exports = {
   name: "beg",
   permissions: ["SEND_MESSAGES"],
@@ -11,6 +12,16 @@ module.exports = {
   description: "beg for coins",
   async execute(client, message, cmd, args, Discord) {
     const randomNumber = Math.floor(Math.random() * 500) + 1;
+    const randompercentage = Math.floor(Math.random() * 100) + 1;
+    const button1 = new MessageButton()
+      .setStyle('green')
+      .setID('mon')
+      .setLabel`Get ${randompercentage} More Xocoins!`
+      .setEmoji("💸")
+
+    const row = new MessageActionRow()
+      .addComponent(button1)
+
     await db.add(`${message.author.username}_begged_total`, 1)
     const totalbegs = await db.get(`${message.author.username}_begged_total`)
     const embed = new Discord.MessageEmbed()
@@ -19,7 +30,20 @@ module.exports = {
       .setDescription(`**You Begged And Got From XOPBOT \`${randomNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}\` Xocoins! 💸**`)
       .setColor(`${color}`)
       .setFooter(`You Have Begged For Over: ${totalbegs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Times!`)
-    message.lineReplyNoMention(embed);
+    message.channel.send(embed, row);
     client.add(message.author.id, randomNumber)
+
+    client.on("clickButton", async (button) => {
+      if (button.id === "mon") {
+        const extrax = new Discord.MessageEmbed()
+          .setTimestamp()
+          .setColor(`${color}`)
+          .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
+          .setDescription(`**You Claimed Extra \`${randompercentage}\` Xocoins! 💸**`)
+          .setFooter(`Have A Little More My Friend! 😃`)
+        message.lineReplyNoMention(extrax)
+      }
+      button.reply.defer()
+    })
   },
 };
