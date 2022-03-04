fe0ad282239b7d5fc2ec1bbec80cb1e55a3721af
HACKERPROTM <brandontorreglosa@gmail.com>
2022-03-04 20:35:54 +0200

    `fix errors`
    So i fixed the errors coming from the bot that made it crash down a lot.

--- commands/Economy/beg.js
+++ commands/Economy/beg.js
@@ -13,49 +13,17 @@ module.exports = {
   async execute(client, message, cmd, args, Discord) {
     const randomNumber = Math.floor(Math.random() * 500) + 1;
     const randompercentage = Math.floor(Math.random() * 100) + 1;
-    const button1 = new MessageButton()
-      .setStyle('green')
-      .setID('mon')
-      .setLabel(`Get ${randompercentage} More Xocoins!`)
-      .setEmoji("💸")
-
-    const button2 = new MessageButton()
-      .setStyle('green')
-      .setID('mon1')
-      .setLabel(`Get ${randompercentage} More Xocoins!`)
-      .setEmoji("💸")
-      .setDisabled(true)
-
-    const row = new MessageActionRow()
-      .addComponent(button1)
-
-    const row2 = new MessageActionRow()
-      .addComponent(button2)
+    const got = randomNumber + randompercentage;
 
     await db.add(`${message.author.username}_begged_total`, 1)
     const totalbegs = await db.get(`${message.author.username}_begged_total`)
     const embed = new Discord.MessageEmbed()
       .setTimestamp()
       .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-      .setDescription(`**You Begged And Got From XOPBOT \`${randomNumber.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}\` Xocoins! 💸**`)
+      .setDescription(`**You Begged And Got From XOPBOT \`${got.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")}\` Xocoins, Including ${randompercentage} Bonus! 💸**`)
       .setColor(`${color}`)
       .setFooter(`You Have Begged For Over: ${totalbegs.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ",")} Times!`)
-    const now = await message.channel.send(embed, row);
-    client.add(message.author.id, randomNumber)
-
-    client.on("clickButton", async (button) => {
-      if (button.id === "mon") {
-        const extrax = new Discord.MessageEmbed()
-          .setTimestamp()
-          .setColor(`${color}`)
-          .setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true }))
-          .setDescription(`**You Claimed Extra \`${randompercentage}\` Xocoins! 💸**`)
-          .setFooter(`Have A Little More My Friend! 😃`)
-        await message.lineReplyNoMention(extrax)
-        client.add(message.author.id, randompercentage)
-        now.edit({ embed: embed, components: [row2] });
-      }
-      button.reply.defer()
-    })
+    message.lineReplyNoMention(embed);
+    client.add(message.author.id, got)
   },
 };
--- commands/Help/config.js
+++ commands/Help/config.js
@@ -15,7 +15,7 @@ module.exports = {
             //.setTitle('__🎮 Games__')
             .setColor(`${color}`)
             .addFields(
-                { name: '__<:dankhype:825049281299611718> Config (9)__', value: '\n[antiwords](https://xopbot.glitch.me/) \n__***Setup A Antiword!***__ \n[antilink](https://xopbot.glitch.me/) \n__***Setup A Antilink!***__ \n[autoany](https://xopbot.glitch.me/) \n__***Catch Any Subreddit!***__ \n[automeme](https://xopbot.glitch.me/) \n__***Setup Automeme!***__ \n[joinchannel](https://xopbot.glitch.me/) \n__***Setup A Join Channel!***__' }
+                { name: '__<:dankhype:825049281299611718> Config (8)__', value: '\n[antiwords](https://xopbot.glitch.me/) \n__***Setup A Antiword!***__ \n[antilink](https://xopbot.glitch.me/) \n__***Setup A Antilink!***__ \n[autoany](https://xopbot.glitch.me/) \n__***Catch Any Subreddit!***__ \n[joinchannel](https://xopbot.glitch.me/) \n__***Setup A Join Channel!***__' }
             )
             .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
 
@@ -25,7 +25,7 @@ module.exports = {
             //.setTitle('__🎮 Games__')
             .setColor(`${color}`)
             .addFields(
-                { name: '__<:dankhype:825049281299611718> Config (9)__', value: '\n[leavechannel](https://xopbot.glitch.me/) \n__***Setup A Leave Channel!***__ \n\[joinmessage](https://xopbot.glitch.me/) \n__***Setup A Join Message!***__ \n\[leavemessage](https://xopbot.glitch.me/) \n__***Setup A Leave Message!***__ \n[setprefix](https://xopbot.glitch.me/) \n__***Set A Custom Prefix!***__' }
+                { name: '__<:dankhype:825049281299611718> Config (8)__', value: '\n[leavechannel](https://xopbot.glitch.me/) \n__***Setup A Leave Channel!***__ \n\[joinmessage](https://xopbot.glitch.me/) \n__***Setup A Join Message!***__ \n\[leavemessage](https://xopbot.glitch.me/) \n__***Setup A Leave Message!***__ \n[setprefix](https://xopbot.glitch.me/) \n__***Set A Custom Prefix!***__' }
             )
             .setFooter(`Created By: ${message.author.tag}`, message.author.displayAvatarURL())
 
