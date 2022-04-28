a1ae235ef8028e7f2a7dcd160358e905814cf149
AryanDaga1 <82522625+AryanDaga1@users.noreply.github.com>
2022-04-28 17:41:36 +0530

    Slash Commands
    Tried to add slash commands

---
+++ SlashCommands/ping.js
@@ -0,0 +1,13 @@
+const fetch = require("node-fetch")
+const { SlashCommandBuilder, SlashCommandStringOption } = require('@discordjs/builders');
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('ping')
+        .setDescription('Replies with pong'),
+   async execute(interaction) {
+        interaction.reply({
+            content: "Pong"
+        })
+}
+};
