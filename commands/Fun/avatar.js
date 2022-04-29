6d0858672f5f127091a015b93de57702fdd82928
HACKERPROTM <brandontorreglosa@gmail.com>
2022-04-29 08:27:42 +0300

    `Update Avatar V2`
    This version contains various image formats

--- commands/Fun/avatar.js
+++ commands/Fun/avatar.js
@@ -1,4 +1,4 @@
-const lineReplyNoMention = require('discord-reply');
+const lineReply = require('discord-reply');
 const color = process.env.Color;
 const { MessageButton, MessageActionRow } = require("discord-buttons");
 module.exports = {
@@ -20,33 +20,33 @@ module.exports = {
         const row1 = new MessageActionRow().addComponents(button4, button5, button6);
         const row2 = new MessageActionRow().addComponents(button4, button5, button6);
         const row3 = new MessageActionRow().addComponents(button4, button5, button6);
-        const avatar_list = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setImage(`${user.displayAvatarURL({ size: 2048, dynamic: true })}`)
+        const avatar_list = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setDescription(`**Other Fotmats Supported: [PNG](${user.displayAvatarURL({ dynamic: true, format: 'png' })})  [JPEG](${user.displayAvatarURL({ dynamic: true, format: 'jpg' })})  [GIF](${user.displayAvatarURL({ dynamic: true, format: 'gif' })})**`).setImage(`${user.displayAvatarURL({ size: 2048, dynamic: true })}`)
         const sendme = await message.channel.send({ embed: avatar_list, components: [row] });
         client.on("clickButton", async (button) => {
             if (button.id === "128") {
                 if (button.clicker.user.id !== message.author.id) {
                     await button.reply.defer();
-                    await button.message.channel.send({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
+                    await button.message.lineReply({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
                 } else if (button.clicker.id === message.author.id) {
-                    const avatar_list1 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setImage(`${user.displayAvatarURL({ size: 128, dynamic: true })}`)
+                    const avatar_list1 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setDescription(`**Other Fotmats Supported: [PNG](${user.displayAvatarURL({ dynamic: true, format: 'png' })})  [JPEG](${user.displayAvatarURL({ dynamic: true, format: 'jpg' })})  [GIF](${user.displayAvatarURL({ dynamic: true, format: 'gif' })})**`).setImage(`${user.displayAvatarURL({ size: 128, dynamic: true })}`)
                     await sendme.edit({ embed: avatar_list1, components: [row1] });
                     await button.reply.defer();
                 }
             } else if (button.id === "256") {
                 if (button.clicker.user.id !== message.author.id) {
                     await button.reply.defer();
-                    await button.message.channel.send({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
+                    await button.message.lineReply({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
                 } else if (button.clicker.id === message.author.id) {
-                    const avatar_list2 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setImage(`${user.displayAvatarURL({ size: 256, dynamic: true })}`)
+                    const avatar_list2 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setDescription(`**Other Fotmats Supported: [PNG](${user.displayAvatarURL({ dynamic: true, format: 'png' })})  [JPEG](${user.displayAvatarURL({ dynamic: true, format: 'jpg' })})  [GIF](${user.displayAvatarURL({ dynamic: true, format: 'gif' })})**`).setImage(`${user.displayAvatarURL({ size: 256, dynamic: true })}`)
                     await sendme.edit({ embed: avatar_list2, components: [row2] });
                     await button.reply.defer();
                 }
             } else if (button.id === "1024") {
                 if (button.clicker.user.id !== message.author.id) {
                     await button.reply.defer();
-                    await button.message.channel.send({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
+                    await button.message.lineReply({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
                 } else if (button.clicker.id === message.author.id) {
-                    const avatar_list3 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setImage(`${user.displayAvatarURL({ size: 1024, dynamic: true })}`)
+                    const avatar_list3 = new Discord.MessageEmbed().setAuthor(`${user.username}`, user.displayAvatarURL({ dynamic: true })).setColor(`${color}`).setTimestamp().setTitle(`Avatar Link`).setURL(`${user.displayAvatarURL({ dynamic: true })}`).setDescription(`**Other Fotmats Supported: [PNG](${user.displayAvatarURL({ dynamic: true, format: 'png' })})  [JPEG](${user.displayAvatarURL({ dynamic: true, format: 'jpg' })})  [GIF](${user.displayAvatarURL({ dynamic: true, format: 'gif' })})**`).setImage(`${user.displayAvatarURL({ size: 1024, dynamic: true })}`)
                     await sendme.edit({ embed: avatar_list3, components: [row3] })
                     await button.reply.defer();
                 }
