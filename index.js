8c330c617c657907da6f6c891a7abbe7f3519cd0
HACKERPROTM <brandontorreglosa@gmail.com>
2022-04-30 10:37:24 +0300

    Update index.js

--- index.js
+++ index.js
@@ -78,6 +78,11 @@ Levels.setURL(process.env.X_MongodbURL);
 
 const { Database } = require("quickmongo")
 const quickmongo = new Database(`${process.env.X_MongodbURL}`)
+quickmongo.connect(process.env.X_MongodbURL, {
+    useNewUrlParser: true, useUnifiedTopology: true, useFindAndModify: true,
+}).then(() => {
+    console.log("Connected To Quick Mongo Database!")
+})
 quickmongo.on("ready", () => {
     console.log("Connected To Quick Mongo Database!")
 });
