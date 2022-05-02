4abbfcb87822e27bb101a5da790c64a256063826
HACKERPROTM <brandontorreglosa@gmail.com>
2022-05-02 21:37:57 +0300

    Update event_handler.js

--- handlers/event_handler.js
+++ handlers/event_handler.js
@@ -2,14 +2,14 @@ const fs = require('fs');
 
 module.exports = (client, Discord) => {
     const load_dir = (dirs) => {
-        const event_files = fs.readdirSync(`./EVENTS/${dirs}`).filter(file => file.endsWith('.js'));
+        const event_files = fs.readdirSync(`./events/${dirs}`).filter(file => file.endsWith('.js'));
 
         for (const file of event_files) {
-            const event = require(`../EVENTS/${dirs}/${file}`);
+            const event = require(`../events/${dirs}/${file}`);
             const event_name = file.split('.')[0];
             client.on(event_name, event.bind(null, Discord, client));
         }
     }
 
-    ['CLIENT', 'GUILD'].forEach(e => load_dir(e));
+    ['client', 'guild'].forEach(e => load_dir(e));
 }
