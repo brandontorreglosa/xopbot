4920eb0f83bfa7d579c0a5d0b2612de1f19aa104
HACKERPROTM <brandontorreglosa@gmail.com>
2022-05-11 20:21:18 +0300

    Update register.js

--- commands/NSFW/register.js
+++ commands/NSFW/register.js
@@ -17,6 +17,7 @@ module.exports = {
         const row = new MessageActionRow().addComponents(button1, button2)
         const row2 = new MessageActionRow().addComponents(button3, button4)
         const row3 = new MessageActionRow().addComponents(button3, button4)
+        const row4 = new MessageActionRow().addComponents(button3, button4)
         const embed = new Discord.MessageEmbed().setColor(`${color}`).setTimestamp().setAuthor(`${member.username}`, member.displayAvatarURL({ dynamic: true })).setDescription(`**By Registering For NSFW Commands You Confirm Your 18. 🔞 \nAnd XOPBOT Is Not Responsible For Any Consequences! Continue?**`)
         const SentMessage = message.channel.send({ embed: embed, components: [row] });
         client.on("clickButton", async (button) => {
@@ -28,13 +29,15 @@ module.exports = {
                     nsfwSchema.findOne({ User: member.id, },
                         async (err, data) => {
                             if (data) {
+                                await button.reply.defer();
+                                await SentMessage.edit({ embed: embed, components: [row2] });
                                 return message.lineReplyNoMention({ content: "**You Have Already Registered For NSFW Commands!**" });
                             }
-                            new nsfwSchema({ User: member.id }).save();
-                            message.lineReply({ content: `**Added ${member} To The NSFW Database! 🔞 \nHave Fun You Little Pervert 😊**` })
                             await button.reply.defer();
-                            await SentMessage.edit({ embed: embed, components: [row2] });
-                        },
+                            await SentMessage.edit({ embed: embed, components: [row3] });
+                            new nsfwSchema({ User: member.id }).save();
+                            return message.lineReply({ content: `**Added ${member} To The NSFW Database! 🔞 \nHave Fun You Little Pervert 😊**` });
+                        }
                     );
                 };
             } else if (button.id === "no") {
@@ -42,10 +45,10 @@ module.exports = {
                     await button.reply.defer();
                     await button.message.lineReply({ content: `**This Is ${user.username}\'s Embed!**`, ephemeral: true });
                 } else if (button.clicker.id === message.author.id) {
-                    const nonukeplz = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**You Cancelled The NSFW Registration Successfully!**`)
-                    message.lineReplyNoMention({ embed: nonukeplz });
                     await button.reply.defer();
-                    await SentMessage.edit({ embed: embed, components: [row3] });
+                    await SentMessage.edit({ embed: embed, components: [row4] });
+                    const nonukeplz = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**You Cancelled The NSFW Registration Successfully!**`)
+                    return message.lineReplyNoMention({ embed: nonukeplz });
                 }
             };
         });
