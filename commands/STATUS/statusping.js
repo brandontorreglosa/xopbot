99acab7b78d791436984b8933dc09dc7033ab6d1
HACKERPROTM <brandontorreglosa@gmail.com>
2022-05-25 16:29:28 +0300

    Update statusping.js

--- commands/STATUS/statusping.js
+++ commands/STATUS/statusping.js
@@ -1,46 +1,44 @@
-// const { MessageEmbed } = require('discord.js');
-// const mongodburl = process.env.X_MongodbURL;
-// //const { Database } = require("quickmongo");
-// //const quick = new Database(mongodburl)
-// const lineReplyNoMention = require('discord-reply');
-// const color = process.env.Color;
-// module.exports = {
-//   name: 'statusping',
-//   permissions: ["SEND_MESSAGES"],
-//   clientpermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
-//   aliases: ['stp', 'sping'],
-//   cooldown: 5,
-//   description: 'Get bot ping For The Server.',
-//   async execute(client, message, cmd, args, Discord) {
-//     try {
-//       const nopr = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**Loading...**`)
-//       const ping = await getDBPingData();
-//       const messagePing = Date.now();
-//       const msg = await message.lineReplyNoMention({ embed: nopr });
-//       const endMessagePing = Date.now() - messagePing;
-//       const embed = new MessageEmbed()
-//         .setAuthor(`🏓 Pong!`, message.author.displayAvatarURL({ dynamic: true }))
-//         .setDescription(`**🌐 Database Ping Data:\n🎣 Fetches Ping: \`${ping.endGet}ms\`\n📝 Wrights Ping: \`${ping.endWright}ms\`\n😕 Average Ping: \`${ping.avarage}ms\`\n💬 Message Ping: \`${endMessagePing}ms\`**`).setColor(`${color}`).setTimestamp();
-//       msg.edit({
-//         content: '',
-//         embed: embed,
-//       });
-//     } catch (err) {
-//       const errorlogs = client.channels.cache.get(errorChannel)
-//       errorlogs.send({ content: `**Error On Status Ping Command!\n\nError:\n\n ${err}**` })
-//     }
-//   }
-// }
-// async function getDBPingData() {
-//   const startGet = Date.now();
-//   await quick.get('QR=.');
-//   const endGet = Date.now() - startGet;
-//   const startWright = Date.now();
-//   await quick.set('QR=.', Buffer.from(startWright.toString()).toString('base64'));
-//   const endWright = Date.now() - startWright;
-//   const avarage = (endGet + endWright) / 2;
-//   try {
-//     quick.delete('QR=.');
-//   } catch (error) { }
-//   return { endGet, endWright, avarage };
-// }
+const { MessageEmbed } = require('discord.js');
+const quick = require("quick.db");
+const lineReplyNoMention = require('discord-reply');
+const color = process.env.Color;
+module.exports = {
+    name: 'statusping',
+    permissions: ["SEND_MESSAGES"],
+    clientpermissions: ["SEND_MESSAGES", "EMBED_LINKS"],
+    aliases: ['stp', 'sping'],
+    cooldown: 5,
+    description: 'Get bot ping For The Server.',
+    async execute(client, message, cmd, args, Discord) {
+        try {
+            const nopr = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**Loading...**`)
+            const ping = await getDBPingData();
+            const messagePing = Date.now();
+            const msg = await message.lineReplyNoMention({ embed: nopr });
+            const endMessagePing = Date.now() - messagePing;
+            const embed = new MessageEmbed()
+                .setAuthor(`🏓 Pong!`, message.author.displayAvatarURL({ dynamic: true }))
+                .setDescription(`**🌐 Database Ping Data:\n🎣 Fetches Ping: \`${ping.endGet}ms\`\n📝 Wrights Ping: \`${ping.endWright}ms\`\n😕 Average Ping: \`${ping.avarage}ms\`\n💬 Message Ping: \`${endMessagePing}ms\`**`).setColor(`${color}`).setTimestamp();
+            msg.edit({
+                content: '',
+                embed: embed,
+            });
+        } catch (err) {
+            const errorlogs = client.channels.cache.get(errorChannel)
+            errorlogs.send({ content: `**Error On Status Ping Command!\n\nError:\n\n ${err}**` })
+        }
+    }
+}
+async function getDBPingData() {
+    const startGet = Date.now();
+    await quick.get('QR=.');
+    const endGet = Date.now() - startGet;
+    const startWright = Date.now();
+    await quick.set('QR=.', Buffer.from(startWright.toString()).toString('base64'));
+    const endWright = Date.now() - startWright;
+    const avarage = (endGet + endWright) / 2;
+    try {
+        quick.delete('QR=.');
+    } catch (error) {}
+    return { endGet, endWright, avarage };
+}
