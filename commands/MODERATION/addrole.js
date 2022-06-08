0df139356bacbe2bdafd6af88e4f05320358651b
HACKERPROTM <brandontorreglosa@gmail.com>
2022-06-08 07:05:07 +0300

    commit

--- commands/MODERATION/addrole.js
+++ commands/MODERATION/addrole.js
@@ -1,4 +1,4 @@
-const lineReplyNoMention = require('discord-reply');
+//const lineReplyNoMention = require('discord-reply');
 const color = process.env.Color;
 module.exports = {
     name: 'addrole',
@@ -8,19 +8,19 @@ module.exports = {
     clientpermissions: ["MANAGE_ROLES", "SEND_MESSAGES", "EMBED_LINKS"],
     async execute(client, message, cmd, args, Discord) {
         const user = message.mentions.users.first();
-        const usertarget = message.guilds.members.cache.get(user.id);
+        const usertarget = message.guilds.members.cache.get(message.user.id);
         if (!user) {
             const nopr = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**\`(prefix)addrole <@user> <@role>\`**`)
-            return message.lineReplyNoMention({ embed: nopr })
+            return message.reply({ embed: nopr })
         }
         args.shift()
         const roleToGive = message.mentions.roles.first()
         if (!roleToGive) {
             const addroleError2 = new Discord.MessageEmbed().setTimestamp().setDescription(`**No Roles Provided!**`).setColor(`${color}`)
-            return message.lineReplyNoMention({ embed: addroleError2 })
+            return message.reply({ embed: addroleError2 })
         }
         usertarget.roles.add(roleToGive.id)
         const embed = new Discord.MessageEmbed().setTimestamp().setColor(`${color}`).setAuthor(`${message.author.username}`, message.author.displayAvatarURL({ dynamic: true })).setDescription(`**Added: \`${roleToGive}\` To \`${user.username}\`**`)
-        message.lineReplyNoMention({ embed: embed })
+        message.reply({ embed: embed })
     }
 }
