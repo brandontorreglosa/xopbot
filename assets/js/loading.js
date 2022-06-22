19dcbcee9b07008f784946cc11682d610d504714
HACKERPROTM <brandontorreglosa@gmail.com>
2022-06-22 18:02:57 +0300

    del html lol

--- assets/js/loading.js
+++
@@ -1,7 +0,0 @@
-var a = document.getElementById("a-load");
-a.style.display = "none";
-setTimeout(() => {
-    const l = document.getElementById("loader");
-    l.style.display = "none";
-    a.style.display = "block";
-}, 5000);
