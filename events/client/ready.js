99981124d57eb687c679f8482faae6565a0fe933
HACKERPROTM <brandontorreglosa@gmail.com>
2022-09-08 19:51:07 +0300

    `Update`

--- events/client/ready.js
+++ events/client/ready.js
@@ -6,7 +6,7 @@ module.exports = (Discord, client) => {
         `${client.channels.cache.size} Channels`,
         `${client.guilds.cache.reduce((a, b) => a + b.memberCount, 0)} Users`,
         'xopbot.glitch.me',
-        'Version 60.0.0'
+        'Version 60.0.1'
     ];
     let i = 0;
     setInterval(() => client.user.setActivity(`x!help | ${activities[i++ % activities.length]}`, { type: 'PLAYING' }), 10000);
